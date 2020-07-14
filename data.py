19d4e52995de8b985236f888dac46c411f2bbfdf
Khương <khuong3493455@gmail.com>
2020-07-14 15:35:00 +0700

    algorithms

--- data.py
+++ data.py
@@ -2,26 +2,48 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 class DATA:
-    def read(file_tail, file_path):
-        if file_tail == 'csv':
-            data = pd.read_csv(file_path,header = None)
-        elif file_tail == 'xlsx':
-            data = pd.read_excel(file_path,header = None)
-        #   Check columns atribute
-        list_name = list(data.iloc[0,:])
-        list_label  =  list(data.iloc[1:,len(list_name)-1].drop_duplicates())
-        if list_name[len(list_name)-1] not in list_label:
-            data = data.drop(0)
-            data.columns = list_name
-        else:
-            list_name_col = []
-            for i in range(len(list_name)):
-                name = 'columns _ '+ str(i)
-                list_name_col.append(name)
-            data.columns = list_name_col
-        m = data.shape[0]
-        n = data.shape[1]
-        return (data, data.columns, n, m)
+    try:
+        def read(file_tail, file_path):
+            if file_tail == 'csv':
+                try:
+                    data = pd.read_csv(file_path,header = None)
+                except(FileNotFoundError):
+                    print ("file_path or file_tail false")
+            elif file_tail == 'xlsx':
+                try:
+                    data = pd.read_excel(file_path,header = None)
+                except(FileNotFoundError):
+                    print("file_path or file_tail false")
+            #   Check columns atribute
+            try:
+                list_name = list(data.iloc[0,:])
+                list_label  =  list(data.iloc[1:,len(list_name)-1].drop_duplicates())
+            except UnboundLocalError:
+                print("variable data not exist")
+            except IndexError:
+                print("index error")
+            try:
+                if list_name[len(list_name)-1] not in list_label:
+                    data = data.drop(0)
+                    data.columns = list_name
+                else:
+                    list_name_col = []
+                    for i in range(len(list_name)):
+                        name = 'columns _ '+ str(i)
+                        list_name_col.append(name)
+                    data.columns = list_name_col
+                m = data.shape[0]
+                n = data.shape[1]
+                columns = data.columns #AttributeErrorUnboundLocalError
+            except IndexError:
+                print("index error")
+            except UnboundLocalError:
+                print("variable data not exist")
+            except AttributeError:
+                print("Data is not data frame")
+            return (data, columns, n, m)
+    except Exception:
+        print("Data error")
     def get_data_train(data, col_feature, col_label, choose_size):
         X = data.iloc[:, col_feature]
         Y = data.iloc[:, col_label]
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from data import DATA
 from svm_tool import SVM_ATHM
-from decision_tree_tool import DECISION_TREE_ATHM
 from naive_bayes_tool import NAIVE_BAYES_ATHM
 from linear_regression_tool import LINEAR_REGRESSION_ATHM
 from sklearn.svm import SVC
@@ -8,7 +7,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn import tree
 
-#   Get form Angular
+#   Get form Angular ---------------------------------------------------------------
 algorithm = "regression"
 # algorithm = "decision_tree"
 # algorithm = "svm"
@@ -21,12 +20,15 @@ file_path = "data_test/abalone.csv" #DATA_TEST for regression
 # col_feature = [0,1,2,3]   # col_feature for classify (iris)
 col_feature = [1,2,3,4,5,6,7] # col_feature for regression (abalone)
 # col_label = 4   # col_label for classify (iris)
-col_label = 8   # col_label for regression (abalone)
+col_label = 9   # col_label for regression (abalone)
 size = 0.3
 #------------------------------------------------------------------------------------
 
+
+
+#   get data, data train, test, create model, evalution, if algorithm is classify then evalution is accuracy else evalution is MSE
 data,columns,n,m = DATA.read(file_tail,file_path)
 X_train, X_test, y_train, y_test = DATA.get_data_train(data,col_feature,col_label,size)
 model,evalution = LINEAR_REGRESSION_ATHM.models(algorithm,LINEAR_REGRESSION_ATHM.clf,X_train,X_test,y_train,y_test)
 
-model.predict(X_test)
+print(model.predict(X_test))
